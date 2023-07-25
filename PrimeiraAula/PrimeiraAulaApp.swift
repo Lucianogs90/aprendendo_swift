48879928170e4cfc7afa31109812b8d7ee4b1ad0
Luciano Silva <lucianogs90@gmail.com>
2023-07-25 19:43:29 -0400

    Initial Commit

---
+++ PrimeiraAula/PrimeiraAulaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeiraAulaApp.swift
+//  PrimeiraAula
+//
+//  Created by Luciano Gomes on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PrimeiraAulaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
